e8c1bdc5aa520f20df4e5e49181504f7d68d6acd
Levi Mwendwa <lvmwendwa4@gmail.com>
2024-07-01 09:49:51 +0300

    completes lab

--- server/app.py
+++ server/app.py
@@ -14,13 +14,39 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
+@app.route('/messages', methods = ['GET', 'POST'])
 def messages():
-    return ''
-
-@app.route('/messages/<int:id>')
+    if request.method == 'GET':
+        message_dict = [message.to_dict() for message in Message.query.all()]
+        return make_response(message_dict, 200)
+    elif request.method == 'POST':
+        message = request.json
+        new_message = Message(
+            body = message.get("body"),
+            username = message.get("username")
+        )
+        db.session.add(new_message)
+        db.session.commit()
+        response = new_message.to_dict()
+        return make_response(response, 200)
+        
+@app.route('/messages/<int:id>', methods= ['PATCH', 'DELETE'])
 def messages_by_id(id):
-    return ''
+    message = Message.query.filter_by(id=id).first()
+    if request.method == 'PATCH':
+        for attr in request.json:
+            setattr(message, attr, request.json.get(attr))
+        response = message.to_dict()
+        status = 200
+        
+
+    elif request.method == 'DELETE':
+        db.session.delete(message)
+        db.session.commit()
+        response = {}
+        status = 200
+        
+    return make_response(response, status)
 
 if __name__ == '__main__':
     app.run(port=5555)
